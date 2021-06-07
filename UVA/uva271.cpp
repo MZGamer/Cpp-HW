3f1f155a1ca02dd1ebadcef2958861eebcd22180
MZGamer <58198679+MZGamer@users.noreply.github.com>
2021-06-07 16:08:51 +0800

    owo

--- UVA/uva271.cpp
+++ UVA/uva271.cpp
@@ -6,23 +6,23 @@ string checked(string t) {
     if(t[0]>='p' && t[0]<='z') {
         return string(1,t[0]);
     } else if(t[0] == 'N') {
-        string Left = "E";
+        string Left = "e";
         if(t.size() -1 > 0)
             Left = checked(t.substr(1,t.size() -1));
         return t[0] + Left;
     } else if(t[0] == 'C' || t[0] == 'D' || t[0] == 'E' || t[0] == 'I') {
-        string Left = "E";
+        string Left = "e";
         if(t.size() -1 > 0) {
             Left = checked(t.substr(1,t.size() -1));
         }
-        string Right = "E";
+        string Right = "e";
         int chk = t.size() - Left.size();
         if(chk-1> 0) {
             Right = checked(t.substr(1 + Left.size(),chk));
         }
         return t[0] + Left + Right;
     }
-    return "E";
+    return "e";
 }
 
 int main() {
